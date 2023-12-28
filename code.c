5ab7b16430e2b5271421129d204437f45e79a312
Sourabh Tiwari <139142656+sketty-intro@users.noreply.github.com>
2023-12-29 00:39:54 +0530

    code.c

---
+++ code.c
@@ -0,0 +1,232 @@
+#include <stdio.h>
+int main() 
+{
+    int score = 0;
+    char answer;
+    printf("LET'S START THE QUIZ !! \n");
+    
+    //Question 1
+    printf("\n1. Who developed the C language?\n");
+    printf("a) Dennis Ritchie\nb) Bill Gates\nc) Steve Jobs\nd) Tim Berners-Lee\n");
+    scanf(" %c", &answer);
+    if (answer == 'a' || answer == 'A') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+
+    //Question 2
+    printf("\n2. When was the C language developed?\n");
+    printf("a) 1972\nb) 1970\nc) 1995\nd) 2000\n");
+    scanf(" %c", &answer);
+    if (answer == 'b' || answer == 'B') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+
+    //Question 3
+    printf("\n3. Which is not a programming language?\n");
+    printf("a) Python\nb) Java\nc) C++\nd) HTML\n");
+    scanf(" %c", &answer);
+    if (answer == 'd' || answer == 'D') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+
+    //Question 4
+    printf("\n4. Who is known as the 'Father of C++'?\n");
+    printf("a) Larry Page\nb) Linus Torvalds\nc) Alan Turing\nd) Bjarne Stroustrup\n");
+    scanf(" %c", &answer);
+    if (answer == 'd' || answer == 'D') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+
+    //Question 5
+    printf("\n5. Which company developed the C# language?\n");
+    printf("a) Google\nb) Microsoft\nc) Apple\nd) IBM\n");
+    scanf(" %c", &answer);
+    if (answer == 'b' || answer == 'B') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+
+    //Question 6
+    printf("\n6. Who created the Python programming language?\n");
+    printf("a) Guido van Rossum\nb) Larry Page\nc) Mark Zuckerberg\nd) Elon Musk\n");
+    scanf(" %c", &answer);
+    if (answer == 'a' || answer == 'A') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+
+    //Question 7
+    printf("\n7. What does CSS stand for in web development?\n");
+    printf("a) Creative Style Sheet\nb) Cascading Style Sheet\nc) Computer Style Sheet\nd) Colorful Style Sheet\n");
+    scanf(" %c", &answer);
+    if (answer == 'b' || answer == 'B') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+
+    //Question 8
+    printf("\n8. Who founded Microsoft?\n");
+    printf("a) Jeff Bezos\nb) Steve Jobs and Steve Wozniak\nc) Larry Page and Sergey Brin\nd) Bill Gates and Paul Allen\n");
+    scanf(" %c", &answer);
+    if (answer == 'd' || answer == 'D') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+
+    //Question 9
+    printf("\n9. Which language is often used for front-end web development?\n");
+    printf("a) Java\nb) Ruby\nc) JavaScript\nd) C++\n");
+    scanf(" %c", &answer);
+    if (answer == 'c' || answer == 'C') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+
+    //Question 10
+    printf("\n10. What does HTML stand for?\n");
+    printf("a) Hyper Training Machine Language\nb) Hyper Text Markup Language\nc) Hyper Transfer Mode Link\nd) High Tech Modern Language\n");
+    scanf(" %c", &answer);
+    if (answer == 'b' || answer == 'B') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+
+    //Question 11
+    printf("\n11. Who created the Java programming language?\n");
+    printf("a) James Gosling\nb) Linus Torvalds\nc) Larry Page\nd) Tim Berners-Lee\n");
+    scanf(" %c", &answer);
+    if (answer == 'a' || answer == 'A') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+
+    //Question 12
+    printf("\n12. Which programming language is commonly used for data analysis and machine learning?\n");
+    printf("a) Java\nb) Python\nc) C#\nd) Swift\n");
+    scanf(" %c", &answer);
+    if (answer == 'b' || answer == 'B') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+
+    //Question 13
+    printf("\n13. Who is the co-founder of Apple Inc. alongside Steve Jobs?\n");
+    printf("a) Bill Gates\nb) Steve Wozniak\nc) Tim Cook\nd) Larry Page\n");
+    scanf(" %c", &answer);
+    if (answer == 'b' || answer == 'B') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+
+    //Question 14
+    printf("\n14. What is the name of the first widely available web browser?\n");
+    printf("a) Safari\nb) Internet Explorer\nc) Mosaic\nd) Firefox\n");
+    scanf(" %c", &answer);
+    if (answer == 'c' || answer == 'C') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+
+    //Question 15
+    printf("\n15. Who is the creator of the Linux operating system?\n");
+    printf("a) Bill Gates\nb) Linus Torvalds\nc) Richard Stallman\nd) Tim Berners-Lee\n");
+    scanf(" %c", &answer);
+    if (answer == 'b' || answer == 'B') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+
+    //Question 16
+    printf("\n16. Which company developed the programming language Swift?\n");
+    printf("a) Google\nb) Microsoft\nc) Apple\nd) IBM\n");
+    scanf(" %c", &answer);
+    if (answer == 'c' || answer == 'C') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+
+    //Question 17
+    printf("\n17. What does SQL stand for in programming?\n");
+    printf("a) Structured Query Language\nb) Simple Question Language\nc) Standard Query Level\nd) Static Query Logic\n");
+    scanf(" %c", &answer);
+    if (answer == 'a' || answer == 'A') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+
+    //Question 18
+    printf("\n18. Which company created the programming language TypeScript?\n");
+    printf("a) Google\nb) Microsoft\nc) Apple\nd) Facebook\n");
+    scanf(" %c", &answer);
+    if (answer == 'b' || answer == 'B') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+
+    //Question 19
+    printf("\n19. Who is the founder of SpaceX and Tesla?\n");
+    printf("a) Larry Page\nb) Elon Musk\nc) Jeff Bezos\nd) Mark Zuckerberg\n");
+    scanf(" %c", &answer);
+    if (answer == 'b' || answer == 'B') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+
+    //Question 20
+    printf("\n20. Which programming language is often used for game development in Unity?\n");
+    printf("a) C#\nb) Java\nc) Python\nd) Ruby\n");
+    scanf(" %c", &answer);
+    if (answer == 'a' || answer == 'A') {
+        printf("Correct!\n");
+        score++;
+    } else {
+        printf("Incorrect.\n");
+    }
+    
+    //Total score
+    printf("\nYour total score: %d out of 20\n", score);
+
+    return 0;
+}
